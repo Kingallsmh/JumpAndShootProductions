4091796154b19d8b4cf13a1da2c5883056f2a9fa
Kingalls <Kingallsmh@gmail.com>
2016-02-03 16:18:12 -0400

    Whole bloody project.

--- desktop/src/com/jump/game/desktop/DesktopLauncher.java
+++ desktop/src/com/jump/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.jump.game.MainGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+                config.title = "Princess Kidnapped!";
+                config.width = 1200;
+                config.height = 600;
 		new LwjglApplication(new MainGame(), config);
 	}
 }
